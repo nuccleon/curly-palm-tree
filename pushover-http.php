cfac321689e36a803ca8de05494aa93b224b0811
nuccleon <github@claus-beckenbauer.de>
2017-01-19 14:07:04 +0100

    changed logfile path to /var/tmp

--- pushover-http.php
+++ pushover-http.php
@@ -12,7 +12,7 @@ use LeonardoTeixeira\Pushover\Sound;
 use LeonardoTeixeira\Pushover\Exceptions\PushoverException;
 
 const CONFIG_FILE = 'pushover-http-config.php';
-const LOG_FILE = 'pushover-http.log';
+const LOG_FILE = '/var/tmp/pushover-http.log';
 const VERBOSITY = DebugLogger::DEBUG;
 
 $logger = new DebugLogger(VERBOSITY, LOG_FILE);
