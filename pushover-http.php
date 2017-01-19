10a44eb927d6a5e41e74741ef8625634dbd5b6d2
nuccleon <github@claus-beckenbauer.de>
2017-01-19 11:52:21 +0100

    Consider configuration file
    Not given mandatory parameters are taken from the configuration file.
    POST/GET parameters will overwrite configuration file entries

--- pushover-http.php
+++ pushover-http.php
@@ -11,11 +11,15 @@ use LeonardoTeixeira\Pushover\Priority;
 use LeonardoTeixeira\Pushover\Sound;
 use LeonardoTeixeira\Pushover\Exceptions\PushoverException;
 
-$verbosity = DebugLogger::DEBUG;
-$logfile = "/var/tmp/pushover-http.log";
-$logger = new DebugLogger($verbosity, $logfile);
-$errorhanlder = new PhpErrorHandler($verbosity, $logfile);
+const CONFIG_FILE = 'pushover-http.config.php';
+const LOG_FILE = 'pushover-http.log';
+const VERBOSITY = DebugLogger::DEBUG;
+
+$logger = new DebugLogger(VERBOSITY, LOG_FILE);
+$errorhanlder = new PhpErrorHandler(VERBOSITY, LOG_FILE);
 $logger->logDebug("Process '".($_SERVER['REQUEST_URI'])."'");
+
+include CONFIG_FILE; // optional include of the configuration after PhpErrorHandler instantiaation
 
 /**
  * HTTP API DESCRIPTION
@@ -24,32 +28,28 @@ $logger->logDebug("Process '".($_SERVER['REQUEST_URI'])."'");
  * At least user, token and priority
  */
 $dateFormat = 'Y-n-d H:i:s';   // supported format, e.g. 2009-02-15 15:16:17
-$mandatory = [ true, 'value' => null ]; // do not change from null to ""!
+$mandatory = [ true, 'value' => null ]; // Comes either form POST/GET or from pushover-http.config.php. Do not change from null to ""!
 $optional = [ false, 'value' => null ]; // null variables will not be set in the message object (see pushover message preparation section)
 $params = [
-   'user'      =>  $mandatory, // the user/group key (not e-mail address) of your user (or you), viewable
+   'user'      =>  $mandatory ,// the user/group key (not e-mail address) of your user (or you), viewable
                                // when logged into our dashboard (often referred to as USER_KEY in our
-                               // documentation and code examples) 
-   'token'     =>  $mandatory, // your application's API token 
+                               // documentation and code examples)
+   'token'     =>  $mandatory, // your application's API token
    'message'   =>  $mandatory, // your message
    'priority'  =>  $optional,  // send as -2 to generate no notification/alert, -1 to always send as a quiet
                                // notification, 1 to display as high-priority and bypass the user's quiet hours,
-                               // or 2 to also require confirmation from the user 
+                               // or 2 to also require confirmation from the user
    'device'    =>  $optional,  // your user's device name to send the message directly to that device,
-                               // rather than all of the user's devices (multiple devices may be separated by a comma) 
-   'title'     =>  $optional,  // your message's title, otherwise your app's name is used 
-   'url'       =>  $optional,  // a supplementary URL to show with your message 
-   'urlTitle'  =>  $optional,  // a title for your supplementary URL, otherwise just the URL is shown 
-   'sound'     =>  $optional,  // The name of one of the sounds supported by device clients to override the user's default sound choice 
+                               // rather than all of the user's devices (multiple devices may be separated by a comma)
+   'title'     =>  $optional,  // your message's title, otherwise your app's name is used
+   'url'       =>  $optional,  // a supplementary URL to show with your message
+   'urlTitle'  =>  $optional,  // a title for your supplementary URL, otherwise just the URL is shown
+   'sound'     =>  $optional,  // The name of one of the sounds supported by device clients to override the user's default sound choice
    'html'      =>  $optional,  // tbd
-   'date'      =>  $optional   // a Unix timestamp of your message's date and time to display to the user, rather than the time your message is received by our API 
+   'date'      =>  $optional,  // a Unix timestamp of your message's date and time to display to the user, rather than the time your message is received by our API
                                // has to be formated as defined in $dateFormat
+   'echo'      =>  $optional   // set this to redirect debug logs to the http response
 ];
-// preset some optional parameters with meaningful values 
-$params['priority']['value'] = Priority::NORMAL;
-$params['sound']['value'] = Sound::PUSHOVER;
-$params['html']['value'] = true;
-$params['date']['value'] = new DateTime();
 
 try {
    /***************************************************************************
@@ -71,24 +71,47 @@ try {
          throw new Exception("Unsupported parameter '".$key."'");
    }
    /***************************************************************************
+    * Preset param with configuration values got from pushover-http.config.php
+    * They will be overwritten with POST/GET values.
+    */
+   if(!file_exists(CONFIG_FILE)) {
+      $logger->logDebug("Dit not found any configuration");
+   } else if(!isset($param_config) || !is_array($param_config)) {
+      $logger->logWarn("Config file does not contain any valid configuration.");
+   } else {
+      foreach($param_config as $key => $value) {
+         if(!isset($params[$key])) { // check if parameter from config file exists in the API params ...
+            $logger->logError("Invalid parameter '".$key."' in config file");
+         } else if($value != "") {  // and copy it to the API params if it has also a valid value
+            $params[$key]['value'] = $value;
+         }
+      }
+   }
+   /***************************************************************************
     * Check if all mandatory parameters are passed to the script.
     * copy parameters from the request query to the internal data
     * represenation ($params) that will be used for the push message.
     */
     foreach($params as $key => &$value) {
       $mandatory = $value[0];
-      if(!isset($request[$key])) { // throw if the API key is not part of the request
-         if($mandatory)
-            throw new Exception("Mandatory parameter '".$key."' is missing");
-      } else if($request[$key] == "") { // throw if the API key is part of the request, but empty. i.e. ?user=
-         if($mandatory)
-            throw new Exception("Mandatory parameter '".$key."' is empty");
+      $isset = isset($request[$key]) && !empty($request[$key]);
+      $configured = !is_null($value['value']);
+      if(!$isset && !$configured) {
+         if($mandatory) {
+            // throw if the mandatory API key is not part of the request and was not set with the configuration
+            $reason = isset($request[$key]) ? 'empty' : 'missing';
+            throw new Exception("Mandatory parameter '".$key."' is ".$reason);
+         }
       } else {
-         $value['value'] = $request[$key];
+         $method = CONFIG_FILE;
          $requirement = $mandatory ? 'mandatory' : 'optional';
-         $logger->logDebug("Got ".$requirement." '".$key."=".$value['value']."' via '".$_SERVER['REQUEST_METHOD']."'");
+         if($isset) {
+            $value['value'] = $request[$key];
+            $method = $_SERVER['REQUEST_METHOD'];
+         }
+         $logger->logDebug("Got ".$requirement." '".$key."=".$value['value']."' via '".$method."'");
 
-         // special handling for date required... need DateTime object         
+         // special handling for date required... need DateTime object
          if($key == 'date') {
             $value['value'] = DateTime::createFromFormat($dateFormat, $request[$key]);
             $errors = DateTime::getLastErrors();
@@ -98,14 +121,14 @@ try {
                $error = reset($errors['warnings']);
             if(isset($error))
                throw new Exception("Optional parameter '".$key."' has to be '".$dateFormat."' formatted (".$error.")");
-         }         
+         }
       }
    }
 } catch (Exception $e) {
    $logger->logFatal($e);
    // return error text to the requestor and return HTTP 'Bad Request'
    echo($e->getMessage());
-   http_response_code(400);   
+   http_response_code(400);
    exit;
 }
 try {
@@ -128,7 +151,7 @@ try {
       $message->setSound($params['sound']['value']);
    if(!is_null($params['html']['value'] != ""))
       $message->setHtml($params['html']['value']);
-   if(!is_null($params['html']['value']))
+   if(!is_null($params['date']['value']))
       $message->setDate($params['date']['value']);
 } catch (Exception $e) {
    $logger->logFatal($e);
@@ -141,7 +164,7 @@ try {
    /***************************************************************************
     * Pushover message sending
     */
-   $client = new Client($params['user']['value'], $params['token']['value']);   
+   $client = new Client($params['user']['value'], $params['token']['value']);
    $client->push($message, $params['device']['value']);
 
    $logger->logDebug("The message has been pushed!");
