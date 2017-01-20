32ceb932bb808397e545d8084b13992099a5fdd4
nuccleon <github@claus-beckenbauer.de>
2017-01-20 11:24:49 +0100

    Handle logfile creation errors

--- PhpErrorHandler.php
+++ PhpErrorHandler.php
@@ -3,14 +3,15 @@ require_once 'DebugLogger.php';
 
 class PhpErrorHandler {
    public function __construct ($verbosity, $logfile) {
-      $this->logger = new DebugLogger($verbosity, $logfile);
       $this->errorhandler = set_error_handler(array($this, 'error_handler'));
+      $this->logger = new DebugLogger($verbosity, $logfile);
    }
    public function __destruct() {
       set_error_handler($this->errorhandler);
    }  
    public function error_handler($errno, $errstr, $errfile, $errline) {
-      $this->logger->logError($errstr.", ".basename($errfile).":".$errline);
+      if(null != $this->logger)
+         $this->logger->logError($errstr.", ".basename($errfile).":".$errline);
    }
    private $errorhandler; 
    private $logger;
